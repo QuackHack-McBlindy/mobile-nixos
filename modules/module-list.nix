413db2aa7bd911a0c7850cfd2d9dd9f043cf54f5
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-01-24 01:38:15 +0100

    Update module-list.nix

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -10,7 +10,7 @@
   ./cross-workarounds.nix
   ./devices-metadata.nix
   ./disk-image.nix
-  ./documentation.nix
+#  ./documentation.nix
   ./eg25-manager.nix
   ./hardware-eink.nix
   ./generated-disk-images.nix
